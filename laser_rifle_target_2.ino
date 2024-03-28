ea527f5141d735bd06c8538b7f47aa94414f9404
Davide Barchetta <dbarchetta@gheppio.hq.mquadro.net>
2024-03-28 13:20:09 +0100

    versione con potenziometro

--- laser_rifle_target_2.ino
+++ laser_rifle_target_2.ino
@@ -7,14 +7,14 @@
 // Ain0 (D 5) PB5  1|    |8  Vcc
 // Ain3 (D 3) PB3  2|    |7  PB2 (D 2) Ain1
 // Ain2 (D 4) PB4  3|    |6  PB1 (D 1) pwm1
-//            GND  4|    |5  PB0 (D 0) pwm0   -> laser
+//            GND  4|    |5  PB0 (D 0) pwm0   -> gong
 //                  +----+
 
-#define AnalogPin 3 //ATTINY 3, NANO A0
+#define analogPin 3 //ATTINY Ain3, NANO A0
+#define potPin 2 //ATTINY Ain2, NANO A1
 #define ding 0 //ATTINY 0, NANO 2
 #define dingTime 30
 #define debounce 250
-#define theshold 80 // max * theshold; 0 < theshold < 1
 
 #define bufferSize 6
 
@@ -24,22 +24,31 @@
 int buffer[bufferSize];
 int index = 0;
 int media = 10;
+int treshold = 80;
 
 void setup() {
-
-  pinMode(AnalogPin,INPUT);
+  ADMUX |= (6<<MUX0);
+  pinMode(analogPin,INPUT);
+  pinMode(potPin,INPUT);
   pinMode(ding,OUTPUT);
   digitalWrite(ding, LOW);
   for(int i = 0;i< bufferSize;i++){
-    buffer[i] =  analogRead(AnalogPin);
+    buffer[i] =  analogRead(analogPin);
   }
   if(info || debug)
     Serial.begin(115200);
 }
 
 void loop() {
-  int sensorValue = analogRead(AnalogPin);
-  if(sensorValue > media + theshold){
+  int sensorValue = analogRead(analogPin);
+  int tmp = analogRead(potPin);
+  treshold = map(tmp, 0, 1023, 0, 255);
+  if(debug){
+    Serial.print(tmp);
+    Serial.print(" ");
+    Serial.println(treshold);
+  }
+  if(sensorValue > media + treshold){
     if(debug){
       Serial.print(media);
       Serial.print(",");
@@ -61,32 +70,7 @@ void loop() {
     sum += buffer[i];
   }
   media = (sum/bufferSize);
-  /*
-  if(sensorValue > media +( media * theshold)){
-    Serial.println("HIT");
-  }
-  if(sensorValue > media + ( media * theshold)){
-    buffer[index] = sensorValue;
-    index += 1;
-    if(index >= bufferSize) { index = 0; }
-    int sum = 0;
-    for (int i = 0; i<bufferSize; i++){
-      sum += buffer[i];
-    }
-    media = (sum/bufferSize);
-  }
-  if(debug){
-    Serial.print(sensorValue);
-    Serial.print(" | ");
-    for (int i = 0; i<bufferSize; i++){
-      Serial.print(buffer[i]);
-      Serial.print(" ");
-    }
-    Serial.print("| ");
-    Serial.print(media);
-    Serial.print("| ");
-    Serial.println(media + media*theshold);
-  }*/
+
   delay(1);
 }
 
